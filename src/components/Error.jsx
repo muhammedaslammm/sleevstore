1b1c49f0808cd530241147b976575b7988c4f6c4
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-01-24 15:52:06 +0530

    saved with single component-error

---
+++ src/components/Error.jsx
@@ -0,0 +1,19 @@
+let Error = () => {
+  return (
+    <div className="error__page w-9/12 mx-[2rem] my-[1.5rem]">
+      <h1 className="error__page__heading text-[1.5rem] font-semibold capitalize">
+        Oops! something went wrong...
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia magni,
+        dolorem odio cum sed incidunt voluptas dolores numquam minus iste
+        cupiditate, quo qui! Ab, aperiam similique corporis reiciendis
+        necessitatibus adipisci qui sequi natus temporibus, quasi dolore. Beatae
+        tempora corporis quam velit earum quidem, voluptate impedit voluptatem.
+        Blanditiis nisi quibusdam dolore.
+      </p>
+    </div>
+  );
+};
+
+export default Error;
